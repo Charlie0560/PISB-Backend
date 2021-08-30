9133a582b00994d6979c90e57fa5e18e6182986d
Charlie0560 <chaitanyalokhande87@gmail.com>
2021-08-30 10:24:45 +0530

    app1

--- app1/urls.py
+++ app1/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.number, name="number"),
     path("numprint/", views.numprint, name="numprint"),
-    path("number/", views.number, name="number")
+
 ]
